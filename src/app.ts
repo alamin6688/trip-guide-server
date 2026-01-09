2b7e899a0fae4ae08026aaf10c36653dce4bf5b6
Alamin <alamin.kzs06@gmail.com>
2026-01-10 00:39:40 +0600

    cors

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ app.post(
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://trip-guide-client.vercel.app"],
     credentials: true,
   })
 );
